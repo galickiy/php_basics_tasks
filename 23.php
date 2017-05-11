52cbc982942bf07ab67e69671cc8a2abdaa08f56
galickiy <nightnomed@gmail.com>
2017-05-11 14:07:21 +0300

    ready

---
+++ 23.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Напишите 3 строки, используя три разные функции для вывода текста.
+ */
+echo 'Hello World!';
+echo "<br>";
+?>
+<?='Hello World!';
+echo "<br>";
+?>
+<?
+printf('Hello World!');
---
+++ 27.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Выполните скрипт по задаче №4 из консоли (необходимо запустить
+ * php скрипт по задаче №4 из командной строки).
+ */
+# linux system
+# /opt/lampp/bin/php /home/%username%/web/php_basic_tasks/4.php
